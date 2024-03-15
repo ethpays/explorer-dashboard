bc03b9661f72022a7e4dc2bce2c89a2ac13d2fb9
fanto6314 <antonio.foresta04@gmail.com>
2024-03-15 14:00:05 +0100

    [+] Update

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import NavBarHero from '../components/NavBarHero';
 import SearchHero from '../components/SearchHero';
 import BlockHero from '../components/BlockHero-1';
+import BlockHero2 from '../components/BlockHero-2';
 
 export default function Home() {
   return (
@@ -12,6 +13,9 @@ export default function Home() {
         <div className="px-72 w-full">
           <SearchHero />
           <BlockHero/>
+          <div className="flex justify-center">
+            <BlockHero2/>
+          </div>
         </div>
       </div>
     </div>
