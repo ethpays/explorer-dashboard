22adc80db88941f59003d2c9c2bf897a58bc3d26
fanto6314 <antonio.foresta04@gmail.com>
2024-03-15 12:03:28 +0100

    [+] Update

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import NavBarHero from '../components/NavBarHero';
 import SearchHero from '../components/SearchHero';
+import BlockHero from '../components/BlockHero-1';
 
 export default function Home() {
   return (
     <div>
       <NavBarHero />
-      <div className="flex flex-col items-start mt-12 text-ethpays_white illustration-section-01">
-        <SearchHero />
+      <div className="flex flex-col items-start mt-12 text-ethpays_white illustration-section-01 w-full">
+        <div className="px-72 w-full">
+          <SearchHero />
+          <BlockHero/>
+        </div>
       </div>
     </div>
   )
